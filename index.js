ba992c09fc832f31ac757acc88af46e6c5d81722
daniellogic144 <73404726+daniellogic144@users.noreply.github.com>
2020-11-01 17:09:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ client.on('ready', () => {
 
 
 const channelId = '768456836566220805'
-
+const guild = client.guilds.cache.get('768456836289921036')
 client.on('guildMemberAdd', (member) => {
   const message = `Hey <@${member.id}>,  welcome to the **Zodiac Esports [BETA]** server:tada::hugging:`
 
   const channel = member.guild.channels.cache.get(channelId)
   member.roles.add("768582572639780884"); 
+  member(guild)
   channel.send(message)
 })
 
